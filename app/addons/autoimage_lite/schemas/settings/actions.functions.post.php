7ccd6f856b50ec585ac84830e5dd34f88f08d127
WSergio@NZXT <sergiu@disruptive.academy>
2015-10-31 11:07:59 +0200

    Updated head comment block

--- app/addons/autoimage_lite/schemas/settings/actions.functions.post.php
+++ app/addons/autoimage_lite/schemas/settings/actions.functions.post.php
@@ -1,16 +1,17 @@
 <?php
 /**
- * AutoImage
+ * HELOstore
  *
  * This source file is part of a commercial software. Only users who have purchased a valid license through
  * https://helostore.com/ and accepted to the terms of the License Agreement can install this product.
  *
- * @category   Zend
- * @package    Zend_Application
+ * @category   Add-ons
+ * @package    HELOstore
  * @copyright  Copyright (c) 2015-2016 HELOstore. (https://helostore.com/)
- * @license    https://helostore.com/legal/license-agreement/     License Agreement
+ * @license    https://helostore.com/legal/license-agreement/   License Agreement
  * @version    $Id$
  */
+
 /**
  * @param null $newStatus
  * @param null $oldStatus
